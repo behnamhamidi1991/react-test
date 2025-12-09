abd4ede9c29a9d94295c78079a2ad6b37b1c227f
behnamhamidi1991 <eslp91@hotmail.com>
2025-12-09 14:04:38 +0330

    Added a p

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const App = () => {
   return <div>
     <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque amet, reprehenderit vero dicta illo, dolorem nesciunt in eum consectetur, voluptatum ratione alias inventore ipsa asperiores tempore soluta eius repudiandae ullam!</p>
   </div>;
 };
 
